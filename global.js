96029c7b379f3cb89a3928e4761cf8c68e11639d
Haris Saif <166474423+harisdsc@users.noreply.github.com>
2025-01-25 21:57:03 -0800

    test

--- global.js
+++ global.js
@@ -13,39 +13,45 @@ function $$(selector, context = document) {
     // currentLink?.classList.add('current');
     
 
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
-
-let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact'},
-    { url: 'resume/', title: 'Resume'},
-    { url: 'https://github.com/harisdsc', title: 'GitHub'}
-  ];
-
-let nav = document.createElement('nav');
-document.body.prepend(nav);
-
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    if (!ARE_WE_HOME && !url.startsWith('http')) {
-        url = '../' + url;
-    } else if (ARE_WE_HOME && url.startsWith('http')) {
-        url = '/portfolio/'
-    }
-    // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-    a.classList.toggle(
-        'current',
-        a.host === location.host && a.pathname === location.pathname
-      );
-   if (a.host !== location.host) {
-    a.target = "_blank"
-   } 
+    
+    let pages = [
+        { url: '', title: 'Home' },
+        { url: 'projects/', title: 'Projects' },
+        { url: 'contact/', title: 'Contact'},
+        { url: 'resume/', title: 'Resume'},
+        { url: 'https://github.com/harisdsc', title: 'GitHub'}
+    ];
+    
+    let nav = document.createElement('nav');
+    document.body.prepend(nav);
+    
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    
+    for (let p of pages) {
+        let url = p.url;
+        let title = p.title;
+        if (!ARE_WE_HOME && !url.startsWith('http')) {
+            url = '../' + url;
+        } else if (ARE_WE_HOME) {
+            if (url === '') {
+                url = '/portfolio/'
+            } else if (!url.startsWith('http')) {
+                url = '/portfolio/' + url;
+            }
+        }
+        }
+        // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+        let a = document.createElement('a');
+        a.href = url;
+        a.textContent = title;
+        nav.append(a);
+        a.classList.toggle(
+            'current',
+            a.host === location.host && a.pathname === location.pathname
+        );
+    if (a.host !== location.host) {
+        a.target = "_blank"
+    } 
   }
 
   document.body.insertAdjacentHTML(
